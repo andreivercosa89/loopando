79ac4ac3fea61aa0347c9da94a5c28b29278b14a
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-26 21:40:34 -0400

    loop

---
+++ loopando/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace loopando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Informe um numero: ");
+            numero = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++){
+                //Console.WriteLine(numero + " x " + i + " = "+ (numero * i));
+                Console.WriteLine(" {0} x {1} = {2}", numero, i, (numero * i));
+            }
+        }
+    }
+}
